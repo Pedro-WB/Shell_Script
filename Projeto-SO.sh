89f4995ac599bd0131795d8a191f66f901a7b3c8
Pedro-WB <128044342+Pedro-WB@users.noreply.github.com>
2023-11-27 11:56:31 -0300

    Update Projeto-SO.sh

--- Projeto-SO.sh
+++ Projeto-SO.sh
@@ -18,15 +18,12 @@ for i in {1..5}; do
     # Nome do arquivo compactado com gzip
     gzip_file="${original_file}.gz"
 
-    # Nome do arquivo compactado com tar
-    tar_file="${original_file}.tar"
-
     # Teste com gzip
     echo "Compactando usando gzip..."
     time gzip "$original_file" >> "$result_file"
     
-    echo "Teste $i - Verificação com gzip -t:" >> "$result_file"
-    gzip -l "$gzip_file" >> "$result_file"
+    echo "Teste $i - Verificação com gzip -l:" >> "$result_file"
+    time gzip -l "$gzip_file" >> "$result_file"
 
     # Verifica se o arquivo compactado com gzip existe
     if [ -e "$gzip_file" ]; then
@@ -43,14 +40,14 @@ for i in {1..5}; do
 
     # Teste com tar
     echo "Teste $i - Verificação com tar -tzvf:" >> "$result_file"
-    time tar -czvf "$tar_file" "$original_file" "segundo_arquivo.txt" >> "$result_file"
+    time tar -czvf "$gzip_file" "$original_file" "segundo_arquivo.txt" >> "$result_file"
 
     # Teste com tar
     echo "Teste $i - Verificação com tar -ztvf:" >> "$result_file"
-    time tar -xzvf "$tar_file" >> "$result_file"
+    time tar -xzvf "$gzip_file" >> "$result_file"
 
     # Verifica se o arquivo compactado com tar existe
-    if [ -e "$tar_file" ]; then
+    if [ -e "$gzip_file" ]; then
         echo "tar: Compactação bem-sucedida."
         echo "Descompactando usando tar..."
         time tar -xvvf "$tar_file" >> "$result_file"
@@ -60,7 +57,7 @@ for i in {1..5}; do
     fi
 
     # Limpa os arquivos temporários criados
-    rm "$original_file" "$gzip_file" "$tar_file" "segundo_arquivo.txt"
+    rm "$original_file" "$gzip_file" "segundo_arquivo.txt"
 
     echo "Teste $i concluído."
 
